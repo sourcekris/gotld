2eae7866c3eda22ff9b8c331331411744781a0f3
Kris Hunt <sourcekris@users.noreply.github.com>
2017-12-29 01:11:09 +1100

    Touch up tests, add some for IDN TLD resolution

--- gotld_test.go
+++ gotld_test.go
@@ -1,15 +1,14 @@
 package gotld
 
 import (
-	//"net"
 	"testing"
 )
 
-var testUrls []string = []string{"www.google.com.hk", "www.forease.net", "com",
-	"www.forease.com.cn", "www.ritto.shiga.jp", "ritto.shiga.jp", "ymm"}
+var testUrls []string = []string{"www.google.com.hk", "www.forease.net", 
+                                 "com",	"www.forease.com.cn", "www.ritto.shiga.jp", 
+                                 "ritto.shiga.jp", "xn--c1avg","قطر"}
 
 func TestGetTld(t *testing.T) {
-	//url := "www.google.com.hk"
 	for _, url := range testUrls {
 		tld, domain, err := GetTld(url)
 		if nil != err {
@@ -18,13 +17,8 @@ func TestGetTld(t *testing.T) {
 		}
 		t.Logf("%s: %v, %s\n", url, tld.Tld, domain)
 	}
-
-	t.Fail()
 }
 
 func TestGetVersion(t *testing.T) {
-
 	t.Logf("%s\n", GetVersion())
-
-	t.Fail()
 }
